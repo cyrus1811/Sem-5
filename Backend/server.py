131bb71be8fb109ef03d6cb5ad644bcfedf51752
PratyayKoley <pratyaykoley@gmail.com>
2024-10-29 11:14:56 +0530

    Backend

---
+++ Backend/server.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI, HTTPException
+import joblib
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load model
+try:
+    scaler = joblib.load("./models/scaler.joblib")
+    type_of_planet = joblib.load("./models/type_of_planet.joblib")
+    print("Models loaded successfully.")
+except Exception as e:
+    print(f"Error loading model: {e}")
+
+class PredictionInput(BaseModel):
+    pl_mass: float
+    pl_radius: float
+    pl_density: float
+
+# Mapping of prediction numbers to labels
+planet_type_labels = {
+    0: "Jovian",
+    1: "Miniterran",
+    2: "Neptunian",
+    3: "Subterran",
+    4: "Superterran",
+    5: "Terran"
+}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World"}
+
+@app.post("/predict-planet-type")
+async def predict_planet_type(input_data: PredictionInput):
+    try:
+        print(input_data)
+        # Prepare data in the correct format
+        data = [[input_data.pl_mass, input_data.pl_radius, input_data.pl_density]]
+        
+        # Check data input
+        print(f"Input data: {data}")
+
+        scaled_data = scaler.transform(data)
+        print(f"Scaled data: {scaled_data}")
+        
+        # Get the prediction and ensure it's an int
+        prediction = type_of_planet.predict(scaled_data)
+        print(f"Raw prediction output: {prediction}")
+        
+        prediction = int(prediction[0])  # Get the first element if it’s an array
+        
+        # Get the label from the dictionary
+        prediction_label = planet_type_labels.get(prediction, "Unknown")
+        return {"prediction": prediction_label}
+    except TypeError as e:
+        raise HTTPException(status_code=400, detail=f"Data type error: {e}")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f"Value error: {e}")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
